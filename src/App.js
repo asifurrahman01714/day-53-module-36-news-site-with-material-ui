5cecd0738917609659684137078e1303d4327cea
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-03-03 23:35:27 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+import TopNews from './components/TopNews/TopNews';
+
+
+
+
+function App() {
+  return (
+    <div >
+     <TopNews></TopNews>
+
+    </div>
+  );
+}
+
+export default App;
